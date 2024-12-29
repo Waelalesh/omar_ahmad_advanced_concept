eadaed770399b0a3e5d378887e5999ad31348d43
waelalash04 <wael.alash04@gmail.com>
2024-12-29 22:21:37 +0300

    - add firebase core - add firebase configuration - change package name - add firebase_options.dart using generate code in firebase cli

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,7 +4,7 @@ import 'package:omar_ahmad_advanced_concept/core/di/dependency_injection.dart';
 import 'package:omar_ahmad_advanced_concept/core/routing/app_router.dart';
 import 'package:omar_ahmad_advanced_concept/doc_app.dart';
 
-void main(i) async {
+void main() async {
   setupGetIt();
   // to fix texts font bug in flutter_screenutils in release mode
   await ScreenUtil.ensureScreenSize();
